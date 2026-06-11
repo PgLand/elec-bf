cf933be4520f6478999d376aa06dd7eab0657baf
PgLand <152286952+PgLand@users.noreply.github.com>
2026-06-11 21:04:03 +0000

    Fix activationUrl in license-config.js
    Updated activationUrl to remove unnecessary text.

--- license-config.js
+++ license-config.js
@@ -2,5 +2,5 @@
 window.WARI_LICENSE = {
   secret: 'CHANGEZ-CE-SECRET-WARI-2026',
   // URL Google Apps Script (/exec) — obligatoire pour bloquer le partage des clés
-  activationUrl: 'https://script.google.com/macros/s/AKfycbzyexinXQy0FcTcLm3RIDcNrYWFNv5xu1TAbAaMoes15xxn292GY6k06pEPCvVOZMM_Bg/execCOLLEZ_VOTRE_URL_GOOGLE_APPS_SCRIPT_ICI'
+  activationUrl: 'https://script.google.com/macros/s/AKfycbzyexinXQy0FcTcLm3RIDcNrYWFNv5xu1TAbAaMoes15xxn292GY6k06pEPCvVOZMM_Bg/exec'
 };
