5920a831cc3cf2fdc0c6748d75ba553f47ec9acc
PgLand <152286952+PgLand@users.noreply.github.com>
2026-06-11 21:53:59 +0000

    Update activationUrl in license-config.js

--- license-config.js
+++ license-config.js
@@ -2,5 +2,5 @@
 window.WARI_LICENSE = {
   secret: 'CHANGEZ-CE-SECRET-WARI-2026',
   // URL Google Apps Script (/exec) — obligatoire pour bloquer le partage des clés
-  activationUrl: 'https://script.google.com/macros/s/AKfycbzYEvdQ2iwSD0TVi8aDEOPqJnWNm1Ue8TcSAA1Q8eRs7keI4ISsL2fNgHDb-jJY2_kTNg/exec'
+  activationUrl: 'https://script.google.com/macros/s/AKfycby7-BMrkIUC0P9L1pUTCBagGw13PxHGlZMya2ekjWQ0-KaScW8AaiOzUQBl-sxbz-iSmg/exec'
 };
